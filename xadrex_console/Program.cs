a716eb9109025047b5dbd0821301ae87a3de308b
alhkanashiro <alhkanashiro@gmail.com>
2019-03-01 14:10:45 -0300

    Impressão do tabuleiro

--- xadrex_console/Program.cs
+++ xadrex_console/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using tabuleiro;
+using xadrex_console;
 
-namespace xadrez_console
+namespace xadrex_console
 {
     class Program
     {
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Posição: " + P);
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
+            Tela.imprimirTabuleiro(tab);
 
             Console.ReadLine();
         }
--- xadrex_console/Tabuleiro/Tabuleiro.cs
+++ xadrex_console/Tabuleiro/Tabuleiro.cs
@@ -2,7 +2,6 @@
 {
     class Tabuleiro
     {
-
         public int linhas { get; set; }
         public int colunas { get; set; }
         private Peca[,] pecas;
@@ -12,6 +11,17 @@
             this.linhas = linhas;
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
+        }
+
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
+        }
+
+        public void colocarPeca(Peca p, Posicao pos)
+        {
+            pecas[pos.linha, pos.coluna] = p;
+            p.posicao = pos;
         }
     }
 }
