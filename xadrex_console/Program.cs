b6f905bf754179aba0d91d1a5744acd6df72f8f1
alhkanashiro <alhkanashiro@gmail.com>
2019-03-01 12:00:21 -0300

    Posição, Tabuleiro, Cor e Peça Adicionado

---
+++ xadrex_console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Posição: " + P);
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Console.ReadLine();
+        }
+    }
+}
