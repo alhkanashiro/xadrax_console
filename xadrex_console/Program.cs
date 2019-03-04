966ecfccff5ee0a88618d30d7ea472cfcf552736
alhkanashiro <alhkanashiro@gmail.com>
2019-03-04 14:55:13 -0300

    Regras de movimentação das peças

--- xadrex_console/Program.cs
+++ xadrex_console/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using tabuleiro;
-using xadrex;
 using xadrez;
-
-namespace xadrex_console
+namespace xadrez_console
 {
     class Program
     {
@@ -12,32 +10,25 @@ namespace xadrex_console
             try
             {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
-
                 while (!partida.terminada)
                 {
+
                     try
                     {
                         Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab);
-                        Console.WriteLine();
-                        Console.WriteLine("Turno: " + partida.turno);
-                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                        Tela.imprimirPartida(partida);
 
                         Console.WriteLine();
                         Console.Write("Origem: ");
                         Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
                         partida.validarPosicaoDeOrigem(origem);
-
                         bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
-
                         Console.Clear();
                         Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
-
                         Console.WriteLine();
                         Console.Write("Destino: ");
                         Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
                         partida.validarPosicaoDeDestino(origem, destino);
-
                         partida.realizaJogada(origem, destino);
                     }
                     catch (TabuleiroException e)
@@ -55,4 +46,3 @@ namespace xadrex_console
         }
     }
 }
-
--- xadrex_console/Tela.cs
+++ xadrex_console/Tela.cs
@@ -1,11 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
 using tabuleiro;
 using xadrez;
 
-namespace xadrex_console
+namespace xadrez_console
 {
     class Tela
     {
+
+        public static void imprimirPartida(PartidaDeXadrez partida)
+        {
+            imprimirTabuleiro(partida.tab);
+            Console.WriteLine();
+            imprimirPecasCapturadas(partida);
+            Console.WriteLine();
+            Console.WriteLine("Turno: " + partida.turno);
+            Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+        }
+
+        public static void imprimirPecasCapturadas(PartidaDeXadrez partida)
+        {
+            Console.WriteLine("Peças capturadas:");
+            Console.Write("Brancas: ");
+            imprimirConjunto(partida.pecasCapturadas(Cor.Branca));
+            Console.WriteLine();
+            Console.Write("Pretas: ");
+            ConsoleColor aux = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            imprimirConjunto(partida.pecasCapturadas(Cor.Preta));
+            Console.ForegroundColor = aux;
+            Console.WriteLine();
+        }
+
+        public static void imprimirConjunto(HashSet<Peca> conjunto)
+        {
+            Console.Write("[");
+            foreach (Peca x in conjunto)
+            {
+                Console.Write(x + " ");
+            }
+            Console.Write("]");
+        }
 
         public static void imprimirTabuleiro(Tabuleiro tab)
         {
@@ -15,21 +50,16 @@ namespace xadrex_console
                 Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.colunas; j++)
                 {
-                    Console.Write("- ");
                     imprimirPeca(tab.peca(i, j));
                 }
                 Console.WriteLine();
             }
             Console.WriteLine("  a b c d e f g h");
         }
-
-
         public static void imprimirTabuleiro(Tabuleiro tab, bool[,] posicoePossiveis)
         {
-
             ConsoleColor fundoOriginal = Console.BackgroundColor;
             ConsoleColor fundoAlterado = ConsoleColor.DarkGray;
-
             for (int i = 0; i < tab.linhas; i++)
             {
                 Console.Write(8 - i + " ");
@@ -51,7 +81,6 @@ namespace xadrex_console
             Console.WriteLine("  a b c d e f g h");
             Console.BackgroundColor = fundoOriginal;
         }
-
         public static PosicaoXadrez lerPosicaoXadrez()
         {
             string s = Console.ReadLine();
@@ -59,7 +88,6 @@ namespace xadrex_console
             int linha = int.Parse(s[1] + "");
             return new PosicaoXadrez(coluna, linha);
         }
-
         public static void imprimirPeca(Peca peca)
         {
             if (peca == null)
@@ -82,6 +110,5 @@ namespace xadrex_console
                 Console.Write(" ");
             }
         }
-
     }
 }
